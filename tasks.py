7b84e2e1367af4bf2a86b43b4362f349d55df8ff
Steven Loria <sloria1@gmail.com>
2017-01-08 17:12:14 -0600

    Update compatiblity with invoke>=0.13.0

--- tasks.py
+++ tasks.py
@@ -9,13 +9,13 @@ docs_dir = 'docs'
 build_dir = os.path.join(docs_dir, '_build')
 
 @task
-def test(watch=False, last_failing=False):
+def test(ctx, watch=False, last_failing=False):
     """Run the tests.
 
     Note: --watch requires pytest-xdist to be installed.
     """
     import pytest
-    flake()
+    flake(ctx)
     args = []
     if watch:
         args.append('-f')
@@ -25,27 +25,27 @@ def test(watch=False, last_failing=False):
     sys.exit(retcode)
 
 @task
-def flake():
+def flake(ctx):
     """Run flake8 on codebase."""
     run('flake8 .', echo=True)
 
 @task
-def clean():
+def clean(ctx):
     run("rm -rf build")
     run("rm -rf dist")
     run("rm -rf webtest-aiohttp.egg-info")
     print("Cleaned up.")
 
 @task
-def readme(browse=False):
+def readme(ctx, browse=False):
     run('rst2html.py README.rst > README.html')
     if browse:
         webbrowser.open_new_tab('README.html')
 
 @task
-def publish(test=False):
+def publish(ctx, test=False):
     """Publish to the cheeseshop."""
-    clean()
+    clean(ctx)
     if test:
         run('python setup.py register -r test sdist bdist_wheel', echo=True)
         run('twine upload dist/* -r test', echo=True)
